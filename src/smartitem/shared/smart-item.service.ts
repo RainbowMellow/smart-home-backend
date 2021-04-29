ec4232ffde57f9c15c8b8f8be13c13437bd89cdf
fauxtistic <56873702+fauxtistic@users.noreply.github.com>
2021-04-29 10:00:34 +0200

    Category added

--- src/smartitem/shared/smart-item.service.ts
+++ src/smartitem/shared/smart-item.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { SmartItem } from './smart-item.model';
+import { Category } from './category.model';
 
 @Injectable()
 export class SmartItemService {
@@ -10,23 +11,39 @@ export class SmartItemService {
   }
 
   seedData(): void {
+    const cat1: Category = {
+      id: 1,
+      name: 'Lamp',
+    };
+    const cat2: Category = {
+      id: 2,
+      name: 'Oven',
+    };
+    const cat3: Category = {
+      id: 3,
+      name: 'Thermostat',
+    };
+
     const item1: SmartItem = {
       id: 1,
-      name: 'HP Lamp',
+      name: 'Bedroom lamp',
+      category: cat1,
       xPos: 1,
       yPos: 1,
       on: false,
     };
     const item2: SmartItem = {
       id: 2,
-      name: 'Bedroom Lamp',
+      name: 'Kitchen oven',
+      category: cat2,
       xPos: 2,
       yPos: 2,
       on: true,
     };
     const item3: SmartItem = {
       id: 3,
-      name: 'Dining Room Lamp',
+      name: 'Living room thermostat',
+      category: cat3,
       xPos: 3,
       yPos: 3,
       on: false,
