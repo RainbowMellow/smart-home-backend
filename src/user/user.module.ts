582d658c750d02c9809995bfd50b34fab42b59a3
fauxtistic <56873702+fauxtistic@users.noreply.github.com>
2021-05-03 08:40:51 +0200

    Small fixes

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UserGateway } from './user.gateway';
 
 @Module({
-  providers: [UserService]
+  providers: [UserService, UserGateway],
 })
 export class UserModule {}
