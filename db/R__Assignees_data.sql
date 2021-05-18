8be84cc89dd29caf25b35a675de24aed3b961b2c
Troels <31724037+tr0els@users.noreply.github.com>
2021-05-18 09:17:52 +0200

    docker with flyway

---
+++ db/R__Assignees_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO [dbo].[Assignee] (Name)
+VALUES
+    ('First task'), ('Second task'); 
---
+++ db/R__Tasks_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO [dbo].[Task] (Description, AssigneeId, DueDate, isCompleted)
+VALUES
+    ('First task', 1, GETDATE(), 0); 
