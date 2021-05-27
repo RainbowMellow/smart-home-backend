952a2642a2c9214cd637104912964581420ff29c
fauxtistic <56873702+fauxtistic@users.noreply.github.com>
2021-05-28 00:45:32 +0200

    For production

--- src/infrastructure/data-source/database.module.ts
+++ src/infrastructure/data-source/database.module.ts
@@ -17,6 +17,7 @@ import { SmartItemEntity } from './entities/smartItem.entity';
         username: configService.get('POSTGRES_USER'),
         password: configService.get('POSTGRES_PASSWORD'),
         database: configService.get('POSTGRES_DB'),
+        url: configService.get('DATABASE_URL'),
         entities: [
           CategoryEntity,
           LogMessageEntity,
@@ -24,6 +25,12 @@ import { SmartItemEntity } from './entities/smartItem.entity';
           SmartItemEntity,
         ],
         synchronize: true,
+        ssl: true,
+        extra: {
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
       }),
     }),
   ],
